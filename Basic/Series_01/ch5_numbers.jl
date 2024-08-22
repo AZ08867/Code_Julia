8ba5f0b1673eed9ccf1f36ea4ecd3a7b0a647ac9
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-22 21:30:13 +0800

    add numbers

---
+++ Basic/Series_01/ch5_numbers.jl
@@ -0,0 +1,40 @@
+# numbers
+
+typeof(-3)
+typeof(3.14)
+
+example::Float32 = 3.14
+typeof(example)
+
+0.1 + 0.2
+1 / 3
+typeof(1 / 3)
+
+1 // 3
+typeof(1 // 3)
+
+1 % 3
+typeof(1 % 3)
+
+1 // 3 + 1 // 7
+pi
+typeof(pi)
+
+round(pi, digits=2)
+round(pi; digits=2)
+
+1000000 == 1_000_000
+
+sqrt(2)
+
+
+# from chapter on basic math
+4 / 2
+5 / 2
+div(4, 2) # div 相当于 取整
+div(5, 2)
+
+
+# \div<tab>
+4 ÷ 2
+5 ÷ 2
