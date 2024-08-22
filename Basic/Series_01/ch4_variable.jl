4a74b41436e41069b06ce00e1516afc6497221a0
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-22 21:30:05 +0800

    add variable

---
+++ Basic/Series_01/ch4_variable.jl
@@ -0,0 +1,20 @@
+# variable
+
+x = 1
+y = 2
+z = x + y
+
+x = 2
+y
+z
+z = x + y
+
+# how to increment
+i = 1
+i += 1
+i = i + 1
+
+# how use variable to perform basic math
+height = 1.75
+weight = 72.0
+bmi = weight / (height * height)
