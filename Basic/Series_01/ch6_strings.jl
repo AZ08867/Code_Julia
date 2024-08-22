8d0a5384c1a0e7acc493f46431825fe28fe0ceed
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-22 21:30:25 +0800

    add strings

---
+++ Basic/Series_01/ch6_strings.jl
@@ -0,0 +1,47 @@
+# characters and strings
+
+typeof('a')
+typeof("b")
+typeof("hello julia")
+
+# how to display quotation marks
+println("doggo says \"book\".")
+println("""doggo says "book".""")
+
+# how to display new lines
+println("1\n2\n3")
+
+# how to display tabs
+println("1\t2\t3")
+
+# how to concatenate strings
+s1 = "hello"
+s2 = "world"
+s1_s2 = s1 * " " * s2
+
+# how to interpolate strings
+s3 = "julia"
+println("hello $s3")
+
+# unicode characters
+
+# \alpha<tab>
+typeof('α')
+typeof("β")
+
+# \:dog:<tab>
+typeof('🐶')
+typeof("🐱")
+
+
+
+α = 1
+🐱 = 3
+α + 🐱
+
+
+π == pi
+
+# \euler<tab>
+typeof(ℯ)
+ℯ
