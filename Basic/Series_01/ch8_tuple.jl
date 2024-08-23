849192c28ded17c9ce9804c02a6cc6f531658c8b
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-23 13:48:27 +0800

    add tuple

---
+++ Basic/Series_01/ch8_tuple.jl
@@ -0,0 +1,25 @@
+# tuple
+
+# construct tuple
+dog = ("Fido", 3, "Golden Retriever")
+typeof(dog)
+
+# access tuple element
+dog[1]
+# tuple elements are immutable
+
+# dog[1] = "doggo"
+
+# tuple element order is immutable
+
+# sort(dog)
+
+# tuple size is immutable
+
+# push!(dog, "doggo@doggo.doggo")
+
+# destructure assignment
+(name, age, breed) = dog
+name
+age
+breed
