544ee7415715f40d22d0621b99ac9289ec7e5504
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-23 14:47:46 +0800

    add library

---
+++ Basic/Series_01/ch21_lilbrary.jl
@@ -0,0 +1,24 @@
+println("example 1")
+
+rand(10)
+
+# Random Numbers Library
+using Random
+Random.seed!(1)
+rand(10)
+
+println("example 2")
+Random.seed!(42)
+
+data = randn(1_000)
+
+function average(x::Vector)
+    sum(x) / length(x)
+end
+average(data)
+
+# Statistics Library
+using Statistics
+mean(data)
+median(data)
+std(data)
